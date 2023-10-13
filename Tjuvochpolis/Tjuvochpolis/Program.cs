bbed289208cde86b8782fb4d251962e8434591b8
Emiltjalol <Emil.ohrstrom@hotmail.com>
2023-10-13 10:26:26 +0200

    Har lagt till en sak till

---
+++ Tjuvochpolis/Tjuvochpolis/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Tjuvochpolis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("test");
+            Console.WriteLine("Nu har jag lagt till denna rad");
+        }
+    }
+}
